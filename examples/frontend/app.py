f2a234d971b9facb244efb17ecd825453282fee2
keneth <stivengarcia7113@gmail.com>
2026-09-10 14:33:06 -0500

    Updated docstring

---
+++ examples/frontend/app.py
@@ -0,0 +1,7 @@
+# Author: Garcia-Cifuentes, K. <ORCID:0009-0001-2607-6359>
+
+# ----------------------------------------------------------------------------------------------------------------------
+# This file contains a Streamlit application that allows users to interact with the seismic review routine without
+# using the command line. The application provides a user-friendly interface for selecting input files,
+# configuring parameters, and visualizing results.
+# ----------------------------------------------------------------------------------------------------------------------
